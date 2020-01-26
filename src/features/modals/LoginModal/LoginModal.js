0103065d7cfbb3a127ea70fb8c084e929d8c9624
tykowale <tykowale@gmail.com>
2020-01-26 14:18:13 -0800

    add buttons to login modal

--- src/features/modals/LoginModal/LoginModal.js
+++ src/features/modals/LoginModal/LoginModal.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Modal } from "semantic-ui-react";
+import { Button, Modal } from "semantic-ui-react";
 
 const LoginModal = () => {
   return (
     <Modal size="mini" open={true}>
-      <Modal.Header>
-        Login
-      </Modal.Header>
-      <Modal.Description>
-        Gonna put a login form here
-      </Modal.Description>
+      <Modal.Header>Login</Modal.Header>
+      <Modal.Description>Gonna put a login form here</Modal.Description>
+      <Modal.Actions>
+        <Button negative>Cancel</Button>
+        <Button> Login </Button>
+      </Modal.Actions>
     </Modal>
   );
 };
