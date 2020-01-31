3fe70296728ee745784bc0fc28be6c440697c5d2
tykowale <tykowale@gmail.com>
2020-01-30 19:08:34 -0800

    working modal manager (finally)

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import MainDashboard from "../../features/mainDashboard/MainDashboard";
 import { Container } from "semantic-ui-react";
 import NavBar from "../../features/nav/NavBar";
+import ModalManager from "../../features/modals/ModalManager/ModalManager";
 
 function App() {
   return (
     <>
+      <ModalManager />
       <NavBar />
       <Container className="main">
         <MainDashboard />
